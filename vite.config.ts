f72005a19a3507a44de62a88639d27e5efd8e35b
tadej <89967862+tadejrebernjak@users.noreply.github.com>
2025-11-03 09:56:38 +0100

    Revert to Tailwind v3

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +13,6 @@ export default defineConfig({
       routesDirectory: './src/pages',
     }),
     react(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
