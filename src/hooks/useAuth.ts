4ae47836090bb1ddc574eb97c0b0f7ab38d35c1e
tadej <89967862+tadejrebernjak@users.noreply.github.com>
2025-12-10 10:12:32 +0100

    task: simplify auth logic

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,5 +2,5 @@ import { AuthContext } from "@povio/ui";
 
 import type { MockAuthUser } from "@/providers/auth.provider";
 
-// Replace type with AuthUser from @/providers/auth.provider after switching from mock auth
+// Replace type with correct type after switching from mock auth
 export const useAuth = () => AuthContext.useAuth<MockAuthUser>();
